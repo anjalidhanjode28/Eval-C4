9a7479be5dba5ae7dd0e208281f0a840e7fdd3e7
anjalidhanjode28 <99669975+anjalidhanjode28@users.noreply.github.com>
2022-05-02 20:13:51 +0530

    Delete C4 directory
    delete

--- C4/components/navbar.js
+++
@@ -1,7 +0,0 @@
-let navbar = () => 
-{
-   return `<h1>News App</h1>
-   <input type="search" placeholder="Search News" id="search_input">`
-}
-
-export { navbar }
--- C4/scripts/news.js
+++
@@ -1,1 +0,0 @@
-// Ude Import export (MANDATORY)
